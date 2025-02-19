e0329dbe91dd5ac077b1b3784210be87a5ec371d
meSaulo <saulo.marcus22@gmail.com>
2025-02-19 15:53:50 -0300

    atualizado

--- java-variaveis-1/variaveis-1/src/main/java/org/example/Main.java
+++ java-variaveis-1/variaveis-1/src/main/java/org/example/Main.java
@@ -5,6 +5,27 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        int idade;
+        int anoFabricao = 2021;
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroCurto;
+
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double NUMERO_PI = 3.14;
+
+        String nome = "Meu Nome";
+
+        nome.concat(" ").concat("Marcus");
+
 
 
 
