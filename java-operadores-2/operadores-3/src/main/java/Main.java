5544bc705a08b0b3a881facf06ae6c0e6aaffaf7
meSaulo <saulo.marcus22@gmail.com>
2025-02-20 15:31:38 -0300

    adicionando conceitos logicos

--- java-operadores-2/operadores-3/src/main/java/Main.java
+++ java-operadores-2/operadores-3/src/main/java/Main.java
@@ -4,8 +4,25 @@ public class Main {
 
         //ternario;
 
+        int a,b;
 
+        a = 5;
+        b = 6;
+        String resultado;
 
+        if(a==b){
+            resultado = "verdadeiro";
+
+        }
+        else{
+            resultado = "falso";
+        }
+
+        System.out.println(resultado);
+
+        String resultadoTernario = a==b?"verdadeiro" : "falso";
+
+        System.out.println(resultadoTernario) ;
 
     }
 
