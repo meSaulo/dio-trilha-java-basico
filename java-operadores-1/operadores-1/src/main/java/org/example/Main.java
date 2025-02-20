58d85c809a0ffd9555c787ea29ca18ff2cafe12c
meSaulo <saulo.marcus22@gmail.com>
2025-02-20 12:32:29 -0300

    Adicionando Modulo Operadores

---
+++ java-operadores-1/operadores-1/src/main/java/org/example/Main.java
@@ -0,0 +1,51 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+       String nome = "Saulo";
+       int idade = 22;
+       double peso = 68.5;
+       char sexo = 'M';
+       boolean doadorOrgao = false;
+
+       // Pessoa pessoa1 = new Pessoa();
+
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao  = 15 / 3;
+        int modulo = 18 % 3;
+
+        double resultado = (10 * 7) + (20/4);
+
+        //classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        String concatenacao ="?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+
+
+    }
+}
