b173ea8e5c02230e6caea29b71667d32be7bfd1d
meSaulo <saulo.marcus22@gmail.com>
2025-02-21 02:08:17 -0300

    atualizando a pasta metodos

--- java-metodo/Metodos/src/main/java/Metodos.java
+++ java-metodo/Metodos/src/main/java/Metodos.java
@@ -3,9 +3,26 @@ public class Metodos {
 
     public static void main(String[] args){
 
-        
+        int n1 = 5;
+        int n2 = 1;
+
+        System.out.println(somar(n1,n2));
+
 
 
     }
+
+
+    public static int somar(int n1, int n2){
+
+        return n1 + n2;
+
+
+
+    }
+
+
+
+
 
 }
