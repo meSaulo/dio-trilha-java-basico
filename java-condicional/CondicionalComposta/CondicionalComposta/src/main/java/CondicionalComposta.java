849f2cc0745cf3bcd753deadb70eadc29bf5784c
meSaulo <saulo.marcus22@gmail.com>
2025-02-25 17:49:23 -0300

    Adicionando condicional composta

---
+++ java-condicional/CondicionalComposta/CondicionalComposta/src/main/java/CondicionalComposta.java
@@ -0,0 +1,13 @@
+public class CondicionalComposta {
+
+    public static void main(String args[]){
+
+
+
+
+
+
+    }
+
+
+}
---
+++ java-condicional/CondicionalSimples/src/main/java/CondicionalSimples.java
@@ -0,0 +1,8 @@
+public class CondicionalSimples {
+
+    public static void main(String[] args){
+
+    }
+
+
+}
