6eba9d1872d353f138a812cfc613703668a9c2a8
meSaulo <saulo.marcus22@gmail.com>
2025-02-28 00:13:39 -0300

    Adicionando Estruturas de repetição Do While

--- java-repeticao/EstruturaWhile/src/main/java/org/example/DoWhile.java
+++ java-repeticao/EstruturaWhile/src/main/java/org/example/DoWhile.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class DoWhile {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        do{
+
+            System.out.println("Telefone tocando....");
+
+        }while(tocando());
+
+        System.out.println("alo !");
+
+    }
+
+    public static boolean tocando(){
+
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("Atendeu ?" + atendeu);
+
+        return ! atendeu;
+    }
+
 }
--- java-repeticao/EstruturaWhile/src/main/java/org/example/Main.java
+++ java-repeticao/EstruturaWhile/src/main/java/org/example/Main.java
@@ -1,12 +1,33 @@
 package org.example;
-
+import java.util.concurrent.ThreadLocalRandom;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        double mesada = 50.0;
+
+        while(mesada > 0){
+            Double valorDoce = geraValorAleatorio();
+
+            if(valorDoce > mesada){
+                valorDoce  = mesada;
+                System.out.println("Doce do valor: " + valorDoce + " Adicionado no carrinho");
+                mesada = mesada - valorDoce;
+            }
+            System.out.println("Mesada:" + mesada);
+            System.out.println("Joãozinho gastou toda a sua mesada em doces");
 
 
 
+
+        }
+
+
+
+    }
+
+    public static double geraValorAleatorio(){
+        return ThreadLocalRandom.current().nextDouble(2,18);
     }
 }
